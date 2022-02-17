f9c626a2dc299b77edad497c04e475df5113aa58
Ricky Thain <rickythain@gmail.com>
2022-02-17 08:03:10 +0000

    create candles on tick stream and update candles on candle chart

--- src/index.js
+++ src/index.js
@@ -2,25 +2,69 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./views/home/Home";
 import Chart from "./components/Chart";
-import { ws } from "./utils/utils";
+import { ws, create_candle_data } from "./utils/utils";
 import { TypeChooser } from "react-stockcharts/lib/helper";
 
 class ChartComponent extends React.Component {
   componentDidMount() {
-    // getData().then((data) => {
-    //   this.setState({ data });
-    //   console.log(data);
-    // });
     ws.onmessage = (msg) => {
       let data = JSON.parse(msg.data);
-      let data_candles = data.candles;
-      data_candles.map((e, i) => {
-        e.date = new Date(e.epoch * 1000);
-        e.volume = 0;
-      });
-      data = data_candles;
-      console.log(data);
-      this.setState({ data });
+
+      if (data.msg_type === "candles") {
+        let data_candles = data.candles;
+        data_candles.map((e, i) => {
+          e.date = new Date(e.epoch * 1000);
+          e.volume = 0;
+        });
+        data = data_candles;
+        // console.log(data);
+        this.setState({ data });
+      }
+
+      // get tick stream
+      if (data.msg_type === "tick") {
+        // set stream id
+        this.setState({ stream_id: data.subscription.id });
+
+        let data_tick = data.tick;
+        // convert epoch to date
+        data_tick.date = new Date(data_tick.epoch * 1000);
+
+        // convert ticks to candle
+        let single_candle = create_candle_data(data_tick);
+
+        // if candle object is not empty, update state data of candles
+        if (Object.keys(single_candle).length !== 0) {
+          console.log("index ", single_candle);
+
+          // combine candles if previous candle has the same minute with the new candle
+          if (
+            this.state.data[this.state.data.length - 1].date.getMinutes() ===
+            single_candle.date.getMinutes()
+          ) {
+            console.log("same second");
+            this.state.data[this.state.data.length - 1].close =
+              single_candle.close;
+            this.state.data[this.state.data.length - 1].high = Math.max(
+              this.state.data[this.state.data.length - 1].high,
+              single_candle.high
+            );
+            this.state.data[this.state.data.length - 1].low = Math.min(
+              this.state.data[this.state.data.length - 1].low,
+              single_candle.low
+            );
+          } else {
+            console.log(
+              "diff ",
+              this.state.data[this.state.data.length - 1].date,
+              single_candle.date
+            );
+            this.state.data.push(single_candle);
+          }
+
+          console.log(this.state.data.length);
+        }
+      }
     };
 
     ws.onopen = function () {
@@ -32,6 +76,23 @@ class ChartComponent extends React.Component {
           end: "latest",
           start: 1,
           style: "candles",
+        })
+      );
+
+      ws.send(
+        JSON.stringify({
+          ticks: "R_50",
+          subscribe: 1,
+        })
+      );
+    };
+
+    // to close tick stream
+    const closeStream = () => {
+      console.log("closing");
+      ws.send(
+        JSON.stringify({
+          forget: this.state.stream_id,
         })
       );
     };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,31 @@
-import { tsvParse, csvParse } from "d3-dsv";
-import { timeParse } from "d3-time-format";
-// import WebSocket from "ws";
-
 const app_id = 1089;
+var arrTicks = [];
+
 export const ws = new WebSocket(
   "wss://ws.binaryws.com/websockets/v3?app_id=" + app_id
 );
+
+export const create_candle_data = (tick) => {
+  let single_candle = {};
+
+  if (arrTicks.length === 0) arrTicks.push(tick);
+  else {
+    if (arrTicks[0].date.getMinutes() === tick.date.getMinutes())
+      arrTicks.push(tick);
+    else {
+      let quotes = arrTicks.map((tick) => tick.quote);
+
+      // set candle data
+      single_candle.date = new Date(arrTicks[0].date.setSeconds(0, 0));
+      single_candle.open = arrTicks[0].quote;
+      single_candle.close = arrTicks[arrTicks.length - 1].quote;
+      single_candle.high = Math.max(...quotes);
+      single_candle.low = Math.min(...quotes);
+      single_candle.volume = 0;
+
+      arrTicks = [];
+    }
+  }
+
+  return single_candle;
+};
