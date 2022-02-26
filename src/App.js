e278d2011cdb150ed48b34f739d846fc96176b41
Balaganabathy Krishnanmoorthy <gana.ganabathy@gmail.com>
2022-02-26 16:39:50 +0800

    static pages done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthModal from "./views/LoginSignup/AuthModal";
+import SetNewPswd from "./views/ForgetPass/SetNewPswd";
+import ForgotPswd from "./views/ForgetPass/ForgotPswd";
+import CheckEmail from "./views/ForgetPass/CheckEmail";
+import SuccessPswd from "./views/ForgetPass/SuccessPswd";
+
+const App = () => {
+  return <AuthModal />;
+};
+
+export default App;
