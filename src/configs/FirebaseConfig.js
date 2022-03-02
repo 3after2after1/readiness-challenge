a03e7ec5516ce5617bbc3cb1ee2ff19151133d7c
Wong Wei Zhe Besquare <wongwz2828@gmail.com>
2022-03-02 08:53:59 +0000

    track config file

---
+++ src/configs/FirebaseConfig.js
@@ -0,0 +1,30 @@
+const FirebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+export default FirebaseConfig;
+
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCul7R9BUWqXQpDadxLPy7wFEmnapAD0mU",
+  authDomain: "trexservices-5fe1c.firebaseapp.com",
+  projectId: "trexservices-5fe1c",
+  storageBucket: "trexservices-5fe1c.appspot.com",
+  messagingSenderId: "540922506377",
+  appId: "1:540922506377:web:b6851e9a471df5cc022a58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
