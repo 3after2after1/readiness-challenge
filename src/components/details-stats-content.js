8ccf74f6fbd32357f3b02d14bce5f733261221de
Ricky Thain <rickythain@gmail.com>
2022-03-08 06:04:15 +0000

    add forex info design

--- src/components/details-stats-content.js
+++ src/components/details-stats-content.js
@@ -1,59 +1,11 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
+import StatsForex from "./stats-forex";
 
 export default function DetailsStatsContent({ statsData }) {
-  React.useEffect(() => {
-    console.log("stats ", statsData);
-    if (statsData) {
-      statsData.pivot.forEach((item) => console.log(item.pivot));
-    } else console.log("null");
-  }, [statsData]);
   return (
     <Card sx={{ minWidth: 200 }}>
-      <div>
-        <h5>Pivot points</h5>
-        {statsData ? (
-          statsData.pivot.map((item) => {
-            return (
-              <div>
-                <p>
-                  {item.pivot} {item.point}
-                </p>
-              </div>
-            );
-          })
-        ) : (
-          <p>loading...</p>
-        )}
-        <h5>Support</h5>
-        {statsData ? (
-          statsData.support.map((item) => {
-            return (
-              <div>
-                <p>
-                  {item.s} {item.value} {item.strength}
-                </p>
-              </div>
-            );
-          })
-        ) : (
-          <p>loading...</p>
-        )}
-        <h5>Resistance</h5>
-        {statsData ? (
-          statsData.resist.map((item) => {
-            return (
-              <div>
-                <p>
-                  {item.r} {item.value} {item.strength}
-                </p>
-              </div>
-            );
-          })
-        ) : (
-          <p>loading...</p>
-        )}
-      </div>
+      <StatsForex statsData={statsData} />
     </Card>
   );
 }
