8dcc8454dd7bca77ee9496d3f7c32252608de909
Victor Lye <viclyemk@gmail.com>
2022-03-16 14:21:33 +0800

    favourite is the watchlist and the card

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 // import CryptoPage from "./views/CryptoPage";
 import WatchList from "./views/WatchList/Watchlist";
 import Favourite from "./views/Favourite/Favourite";
+import Navbar2 from "./component/Navbar/Navbar2";
+import Footer2 from "./component/Footer/Footer2";
+import { Box } from "@mui/material";
 
 const App = () => {
   return (
     <div>
-      <Favourite />
+      <Box style={{}}>
+        <Navbar2 />
+        <Favourite />
+        <Footer2 />
+      </Box>
     </div>
   );
 };
--- src/views/Favourite/Favourite.js
+++ src/views/Favourite/Favourite.js
@@ -6,153 +6,231 @@ import CardMedia from "@mui/material/CardMedia";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import { blue } from "@mui/material/colors";
-import AddBoxIcon from "@mui/icons-material/AddBox";
-import { makeStyles } from "@material-ui/core/styles";
 import PlaylistRemoveIcon from "@mui/icons-material/PlaylistRemove";
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 300,
-    borderRadius: 300,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 20,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+import Footer2 from "../../component/Footer/Footer2";
+import { Box, Button, Typography } from "@mui/material";
+import CardFav from "./CardFav";
 
 function Favourite() {
   return (
-    <div className="watchlist-page">
-      <div
-        id="fav-box"
+    <>
+      <Box
+        id="watchlist-page"
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
+          backgroundColor: "#f9f7f7",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          paddingBottom: "25px",
         }}
       >
-        <Card
-          id="w-card-box"
-          sx={{
-            width: 300,
-            margin: "10px 0 0 0 ",
+        <Box style={{ textAlign: "center", paddingTop: "20px" }}>
+          <Typography
+            id="watchlistTitle"
+            variant="h5"
+            // component="div"
+            sx={{
+              fontFamily: "League Spartan",
+              color: "black",
+              fontWeight: "bold",
+            }}
+          >
+            My Watchlist
+          </Typography>
+        </Box>
+        <Box
+          id="watchlistForexBox"
+          style={{
+            textAlign: "start",
+            paddingTop: "20px",
           }}
         >
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: blue[500] }} aria-label="Logo">
-                L
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="add to watchlist">
-                <PlaylistRemoveIcon />
-              </IconButton>
-            }
-            title="What forex"
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image="/static/images/cards/paella.jpg"
-            alt="Insert chart"
-          />
-        </Card>
+          <Typography
+            id="watchlistForexTitle"
+            variant="h4"
+            // component="div"
+            sx={{
+              fontFamily: "Bree Serif",
+              color: "black",
+              fontWeight: "",
+            }}
+          >
+            Forex
+          </Typography>
+        </Box>
+        <Box>
+          <Box
+            id="favourite-box"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              paddingBottom: "20px",
+            }}
+          >
+            <Card
+              id="w-empty-card-box"
+              sx={{
+                width: 300,
+                margin: "10px 0 0 0 ",
+                height: 200,
+              }}
+            >
+              <Box
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  textAlign: "center",
+                  height: "100px",
+                  // alignItems: "center",
+                  paddingTop: "30px",
+                }}
+              >
+                <Typography
+                  id=""
+                  variant="h6"
+                  // component="div"
+                  sx={{
+                    fontFamily: "Bree Serif",
+                    color: "#999",
+                    fontWeight: "",
+                    width: 200,
+                  }}
+                >
+                  Browse and add a forex to your watchlist now.
+                </Typography>
+              </Box>
+              <Box
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "50px",
+                }}
+              >
+                <Button
+                  id=""
+                  variant="contained"
+                  style={{
+                    backgroundColor: "#FFCE45",
+                    color: "#184D47",
+                    fontSize: "1rem",
+                    fontFamily: "Bree Serif",
+                    fontWeight: "bold",
+                    height: "30px",
+                  }}
+                >
+                  Go to Forex
+                </Button>
+              </Box>
+            </Card>
 
-        <Card
-          id="w-card-box"
-          sx={{
-            width: 300,
-            margin: "10px 0 0 0 ",
-          }}
-        >
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: blue[500] }} aria-label="Logo">
-                L
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="add to watchlist">
-                <PlaylistRemoveIcon />
-              </IconButton>
-            }
-            title="What forex"
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image="/static/images/cards/paella.jpg"
-            alt="Insert chart"
-          />
-        </Card>
+            <CardFav />
 
-        <Card
-          id="w-card-box"
-          sx={{
-            width: 300,
-            margin: "10px 0 0 0 ",
-          }}
-        >
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: blue[500] }} aria-label="Logo">
-                L
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="add to watchlist">
-                <PlaylistRemoveIcon />
-              </IconButton>
-            }
-            title="What forex"
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image="/static/images/cards/paella.jpg"
-            alt="Insert chart"
-          />
-        </Card>
+            <CardFav />
 
-        <Card
-          id="w-card-box"
-          sx={{
-            width: 300,
-            marginTop: "10px",
+            <CardFav />
+          </Box>
+        </Box>
+        <Box
+          style={{
+            textAlign: "start",
+            paddingTop: "20px",
           }}
         >
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: blue[500] }} aria-label="Logo">
-                L
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="add to watchlist">
-                <PlaylistRemoveIcon />
-              </IconButton>
-            }
-            title="What forex"
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image="/static/images/cards/paella.jpg"
-            alt="Insert chart"
-          />
-        </Card>
-      </div>
-    </div>
+          <Typography
+            id="watchlistCryptoTitle"
+            variant="h4"
+            // component="div"
+            sx={{
+              fontFamily: "Bree Serif",
+              color: "black",
+              fontWeight: "",
+            }}
+          >
+            Crypto
+          </Typography>
+        </Box>
+        <Box
+          id="favourite-box"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingBottom: "20px",
+          }}
+        >
+          <Card
+            id="w-empty-card-box"
+            sx={{
+              width: 300,
+              margin: "10px 0 0 0 ",
+              height: 200,
+            }}
+          >
+            <Box
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                textAlign: "center",
+                height: "100px",
+                // alignItems: "center",
+                paddingTop: "30px",
+              }}
+            >
+              <Typography
+                id=""
+                variant="h6"
+                // component="div"
+                sx={{
+                  fontFamily: "Bree Serif",
+                  color: "#999",
+                  fontWeight: "",
+                  width: 200,
+                }}
+              >
+                Browse and add a crypto to your watchlist now.
+              </Typography>
+            </Box>
+            <Box
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "50px",
+              }}
+            >
+              <Button
+                id=""
+                variant="contained"
+                style={{
+                  backgroundColor: "#FFCE45",
+                  color: "#184D47",
+                  fontSize: "1rem",
+                  fontFamily: "Bree Serif",
+                  fontWeight: "bold",
+                  height: "30px",
+                }}
+              >
+                Go to Crypto
+              </Button>
+            </Box>
+          </Card>
+          <CardFav />
+          <CardFav />
+          <CardFav />
+          <CardFav />
+          <CardFav />
+          <CardFav />
+          <CardFav />
+          <CardFav />
+        </Box>
+      </Box>
+      {/* <Box style={{ paddingTop: "24px", backgroundColor: "#f9f7f7" }}>
+        <Footer2 />
+      </Box> */}
+    </>
   );
 }
 
